920bdbdbdc85ca6e78a8a13752fdd5c4138f7bb1
IT21295560 <99237480+IT21295560@users.noreply.github.com>
2022-02-08 09:00:15 +0000

    Tute 1,2,3,4

--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,19 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
-  
+  int num1,num2;
+  float average;
+  int sum;
+
+  printf("Enter mark 1 : ");
+  scanf("%d",&num1);
+
+  printf("Enter mark 2 : ");
+  scanf("%d",&num2);
+
+  sum = num1 + num2;
+  average = sum/2.0;
+
+  printf("Average is %.2f",average);
   return 0;
-}
+  }
